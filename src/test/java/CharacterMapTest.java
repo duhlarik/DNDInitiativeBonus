741a84cf8e92e1ffbd6de832c993b7fbed264b5b
DeeAnn Uhlarik <duhlarik@users.noreply.github.com>
2018-01-08 20:23:19 -0500

    Removed ignored files

--- src/test/java/CharacterMapTest.java
+++ src/test/java/CharacterMapTest.java
@@ -2,6 +2,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class CharacterMapTest {
@@ -31,7 +32,23 @@ public class CharacterMapTest {
     @Test
     public void dungeonMasterIsAbleToSortTheCharacterMapByInitBonus() {
 
+        CharacterMap characterMap = new CharacterMap();
+
+        DNDCharacter Eroang = new DNDCharacter("Eroang", 13);
+        DNDCharacter Trerrathiel = new DNDCharacter("Trerrathiel", -2);
+        DNDCharacter Thidus = new DNDCharacter("Thidus", 7);
+
+        characterMap.updateCharacterMap(Eroang);
+        characterMap.updateCharacterMap(Trerrathiel);
+        characterMap.updateCharacterMap(Thidus);
+
+        Map<String, Integer> expectedMap = new LinkedHashMap<>();
+        expectedMap.put("Trerrathiel", -2);
+        expectedMap.put("Thidus", 7);
+        expectedMap.put("Eroang", 13);
+
+        characterMap.sortCharacterMapByValue(characterMap);
+
+        Assert.assertEquals(characterMap.getCharacterMap(), expectedMap);
     }
-
-
 }
